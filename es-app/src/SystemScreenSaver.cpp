eedcf9d165bfe0f3d22b629ff046fb6c5546fcb3
Ryan Coe <bluemrp9@gmail.com>
2022-04-18 12:43:40 -0700

    fix opacity behavior in SystemScreenSaver
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>

--- es-app/src/SystemScreenSaver.cpp
+++ es-app/src/SystemScreenSaver.cpp
@@ -16,9 +16,7 @@
 #include <time.h>
 #include <unordered_map>
 
-#define FADE_TIME 			300
-
-static int lastIndex = 0;
+#define FADE_TIME 300
 
 SystemScreenSaver::SystemScreenSaver(Window* window) :
 	mWindow(window),
@@ -45,6 +43,7 @@ bool SystemScreenSaver::isScreenSaverActive()
 void SystemScreenSaver::startScreenSaver()
 {
 	mState = STATE_FADE_OUT;
+	mOpacity = 0.0f;
 }
 
 void SystemScreenSaver::stopScreenSaver()
@@ -58,7 +57,7 @@ void SystemScreenSaver::renderScreenSaver()
 	{
 		std::string screensaver_behavior = Settings::getInstance()->getString("ScreenSaverBehavior");
 		Renderer::setMatrix(Transform4x4f::Identity());
-		unsigned char color = screensaver_behavior == "dim" ? 0x000000A0 : 0x000000FF;
+		unsigned int color = (int)((screensaver_behavior == "dim" ? 0x000000A0 : 0x000000FF)*mOpacity);
 		Renderer::drawRect(0.0f, 0.0f, Renderer::getScreenWidth(), Renderer::getScreenHeight(), color, color);
 	}
 }
@@ -72,8 +71,6 @@ void SystemScreenSaver::update(int deltaTime)
 		if (mOpacity >= 1.0f)
 		{
 			mOpacity = 1.0f;
-
-			// Update to the next state
 			mState = STATE_ACTIVE;
 		}
 	}
