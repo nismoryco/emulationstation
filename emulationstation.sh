18c861ef0459ec6029a62bd672ef112d85c106ce
Ryan Coe <bluemrp9@gmail.com>
2023-02-20 17:04:28 -0800

    update emulationstation.sh
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>

--- emulationstation.sh
+++ emulationstation.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 
-esdir="$(dirname $0)"
-while true; do
-    rm -f /tmp/es-restart /tmp/es-sysrestart /tmp/es-shutdown
-    "$esdir/emulationstation" "$@"
-    ret=$?
-    [ -f /tmp/es-restart ] && continue
-    if [ -f /tmp/es-sysrestart ]; then
-        rm -f /tmp/es-sysrestart
-        sudo reboot
-        break
-    fi
-    if [ -f /tmp/es-shutdown ]; then
-        rm -f /tmp/es-shutdown
-        sudo poweroff
-        break
-    fi
-    break
-done
-exit $ret
+export HOME=/root
+
+/usr/bin/tput civis
+
+/usr/bin/emulationstation.bin
+RET=$?
+
+case "$RET" in
+  0)
+	DEPTH2=`/usr/sbin/fbset | head -3 | tail -1 | cut -d " " -f 10`
+	if [ "$DEPTH2" == "8" ]; then
+		DEPTH1=16
+	elif [ "$DEPTH2" == "16" ] || [ "$DEPTH2" == "32" ]; then
+		DEPTH1=8
+	else
+		DEPTH1=8
+		DEPTH2=16
+	fi
+	/usr/sbin/fbset -depth $DEPTH1 > /dev/null 2>&1
+	/usr/sbin/fbset -depth $DEPTH2 > /dev/null 2>&1
+	/usr/bin/sleep 2
+	;;
+  1)
+	/usr/sbin/poweroff
+	;;
+  2)
+	/usr/sbin/reboot
+	;;
+  3)
+	/usr/bin/tput cnorm
+	export PS1='\u@\h:\w# '
+	cd $HOME
+	echo
+	exec /bin/sh --login
+	;;
+esac
+
+exit 0
